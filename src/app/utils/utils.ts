4a60c9554de498db37d257411c227956d5141d6b
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:42:12 +0100

    Add getAverageRating utils function

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,3 +1,5 @@
+import { Review } from '../services/firestore/credentials.type';
+
 export function capitalize(input: string) {
   const trimmed = input.trim();
   return `${trimmed[0].toUpperCase()}${trimmed.slice(1).toLowerCase()}`;
@@ -7,4 +9,10 @@ export function capitalizeCity(input: string) {
   let split = input.split(' ');
   split = split.map((string) => capitalize(string));
   return split.join(' ');
+}
+
+function getAverageRating(reviews: Review[]): number {
+  const sum = reviews.reduce((acc, cur) => (acc += cur['rating']), 0);
+  const average = sum / reviews.length;
+  return average ? average : 0;
 }
