2e72560a883b8aec959b03c0217096c2b35341bb
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 13:13:45 +0100

    Add login page reactive form

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class LoginPage implements OnInit {
+  credentials!: FormGroup;
+  private router: Router = inject(Router);
+  private authService: AuthService = inject(AuthService);
+  private alertController: AlertController = inject(AlertController);
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.credentials = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
   }
-
 }
