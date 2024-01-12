4b86420c9b8b562af66e122d87c2d6b0efcaa656
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:41:23 +0100

    Add capitalizeCity utils function

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -2,3 +2,9 @@ export function capitalize(input: string) {
   const trimmed = input.trim();
   return `${trimmed[0].toUpperCase()}${trimmed.slice(1).toLowerCase()}`;
 }
+
+export function capitalizeCity(input: string) {
+  let split = input.split(' ');
+  split = split.map((string) => capitalize(string));
+  return split.join(' ');
+}
