98fd6381994c81d9cdd9d5c7b8c90a59a9896cfc
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:42:46 +0100

    Add sortCraftsman utils function

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,4 +1,4 @@
-import { Review } from '../services/firestore/credentials.type';
+import { Craftsman, Review } from '../services/firestore/credentials.type';
 
 export function capitalize(input: string) {
   const trimmed = input.trim();
@@ -9,6 +9,44 @@ export function capitalizeCity(input: string) {
   let split = input.split(' ');
   split = split.map((string) => capitalize(string));
   return split.join(' ');
+}
+
+export function sortCraftsmen(
+  craftsmanArr: Craftsman[],
+  sortBy: string,
+  orderBy: string
+): Craftsman[] {
+  let sortedCraftsmen: Craftsman[] = [];
+  if (sortBy === 'Last Name') {
+    if (orderBy === 'asc') {
+      sortedCraftsmen = craftsmanArr.sort((a, b) =>
+        a.lastName < b.lastName ? -1 : a.lastName === b.lastName ? 0 : 1
+      );
+    } else if (orderBy === 'desc') {
+      sortedCraftsmen = craftsmanArr.sort((a, b) =>
+        a.lastName < b.lastName ? 1 : a.lastName === b.lastName ? 0 : -1
+      );
+    }
+  } else if (sortBy === 'Rating') {
+    if (orderBy === 'asc') {
+      sortedCraftsmen = craftsmanArr.sort((a, b) =>
+        getAverageRating(a.reviews) < getAverageRating(b.reviews)
+          ? -1
+          : getAverageRating(a.reviews) === getAverageRating(b.reviews)
+          ? 0
+          : 1
+      );
+    } else if (orderBy === 'desc') {
+      sortedCraftsmen = craftsmanArr.sort((a, b) =>
+        getAverageRating(a.reviews) < getAverageRating(b.reviews)
+          ? 1
+          : getAverageRating(a.reviews) === getAverageRating(b.reviews)
+          ? 0
+          : -1
+      );
+    }
+  }
+  return sortedCraftsmen;
 }
 
 function getAverageRating(reviews: Review[]): number {
