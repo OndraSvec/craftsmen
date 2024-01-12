a7c2f8c42feea14bf5217802d4426212406e07d3
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:32:07 +0100

    Add Credentials type

---
+++ src/app/services/firestore/credentials.type.ts
@@ -0,0 +1,11 @@
+export type Credentials = {
+  city?: string;
+  company?: string;
+  CRN?: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  profession?: string;
+  type: string;
+};
