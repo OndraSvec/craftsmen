a3a9ee8ce4f54d02b4b40f6c4959dcb0539025ee
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:44:08 +0100

    Add firestore service addUser function

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
-import { Firestore } from '@angular/fire/firestore';
+import { Auth, User } from '@angular/fire/auth';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 import { Subject } from 'rxjs';
-import { Craftsman } from './credentials.type';
+import { Craftsman, Credentials } from './credentials.type';
+import { capitalize, capitalizeCity } from 'src/app/utils/utils';
 
 @Injectable({
   providedIn: 'root',
@@ -14,4 +15,37 @@ export class FirestoreService {
   constructor() {}
 
   craftsmenChanged = new Subject<Promise<Craftsman[]>>();
+
+  async addUser(user: User, obj: Credentials) {
+    try {
+      const { firstName, lastName, email, type } = obj;
+
+      const userDocRef = doc(this.firestore, `users/${user.uid}`);
+      await setDoc(
+        userDocRef,
+        obj.profession && obj.city && obj.company && obj.CRN
+          ? {
+              firstName: capitalize(firstName),
+              lastName: capitalize(lastName),
+              email,
+              type: capitalize(type),
+              profession: capitalize(obj.profession),
+              company: obj.company,
+              city: capitalizeCity(obj.city),
+              CRN: +obj.CRN,
+            }
+          : {
+              firstName: capitalize(firstName),
+              lastName: capitalize(lastName),
+              email,
+              type: capitalize(type),
+            },
+        { merge: true }
+      );
+
+      return userDocRef;
+    } catch (error) {
+      return false;
+    }
+  }
 }
