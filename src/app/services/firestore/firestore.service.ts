cb1c4e5482cb5a9d0939962a54cc4ad26d05e755
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:37:11 +0100

    Add firestore service craftsmenChanged rxjs Subject

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+import { Subject } from 'rxjs';
+import { Craftsman } from './credentials.type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirestoreService {
+  private auth: Auth = inject(Auth);
+  private firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  constructor() {}
+
+  craftsmenChanged = new Subject<Promise<Craftsman[]>>();
 }
