624b0df98aca2a7bf56a9b078781399ae1f47007
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 13:04:09 +0100

    Add register page reactive form

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,20 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [CommonModule, ReactiveFormsModule],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage implements OnInit, OnDestroy {
+  credentials!: FormGroup;
+  private formSub!: Subscription;
+  private router: Router = inject(Router);
+  private authService: AuthService = inject(AuthService);
+  private alertController: AlertController = inject(AlertController);
 
-  constructor() { }
+  userTypes = ['Reviewer', 'Craftsman'];
 
-  ngOnInit() {
+  constructor() {}
+
+  get type() {
+    return this.credentials.get('type');
   }
 
+  get profession() {
+    return this.credentials.get('profession');
+  }
+
+  get company() {
+    return this.credentials.get('company');
+  }
+
+  get CRN() {
+    return this.credentials.get('CRN');
+  }
+
+  get city() {
+    return this.credentials.get('city');
+  }
+
+  ngOnInit() {
+    this.credentials = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      type: new FormControl('', [Validators.required]),
+      profession: new FormControl(''),
+      company: new FormControl(''),
+      city: new FormControl(''),
+      CRN: new FormControl(''),
+    });
+
+    if (this.type) {
+      this.formSub = this.type.valueChanges.subscribe((val) => {
+        if (val === 'Craftsman') {
+          this.profession?.setValidators([Validators.required]);
+          this.company?.setValidators([Validators.required]);
+          this.city?.setValidators([Validators.required]);
+          this.CRN?.setValidators([
+            Validators.required,
+            Validators.pattern('^[0-9]+$'),
+          ]);
+        } else {
+          this.profession?.clearValidators();
+          this.company?.clearValidators();
+          this.city?.clearValidators();
+          this.CRN?.clearValidators();
+        }
+        this.profession?.updateValueAndValidity();
+        this.company?.updateValueAndValidity();
+        this.city?.updateValueAndValidity();
+        this.CRN?.updateValueAndValidity();
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.formSub.unsubscribe();
+  }
 }
