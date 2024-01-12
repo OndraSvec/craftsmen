24f30524590f742ba3007b59ab616788843c91ac
Ondra Svec <ondrasvecveslo@gmail.com>
2024-01-12 11:20:17 +0100

    Initialize firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'craftsmen-prod',
+    appId: '1:396567455921:web:5da8094d2344d862e01a90',
+    storageBucket: 'craftsmen-prod.appspot.com',
+    apiKey: 'AIzaSyBhMFOcc3oscz-aLbsQ4Zyf7Ufs6RQL9dY',
+    authDomain: 'craftsmen-prod.firebaseapp.com',
+    messagingSenderId: '396567455921',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'craftsmen-prod',
+    appId: '1:396567455921:web:5da8094d2344d862e01a90',
+    storageBucket: 'craftsmen-prod.appspot.com',
+    apiKey: 'AIzaSyBhMFOcc3oscz-aLbsQ4Zyf7Ufs6RQL9dY',
+    authDomain: 'craftsmen-prod.firebaseapp.com',
+    messagingSenderId: '396567455921',
+  },
 };
 
 /*
